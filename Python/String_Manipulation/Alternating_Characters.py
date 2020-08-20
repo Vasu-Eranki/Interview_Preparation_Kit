e1e3c27b64685ddc3dede10671f253f6cd7476b7
Vasu Eranki <vasueranki@gmail.com>
2020-08-20 19:27:01 +0530

    Update Alternating_Characters.py

--- Python/String_Manipulation/Alternating_Characters.py
+++ Python/String_Manipulation/Alternating_Characters.py
@@ -8,18 +8,8 @@ import sys
 
 # Complete the alternatingCharacters function below.
 def alternatingCharacters(s):
-    a=s.count('A')
-    b=s.count('B')
-    #print(a,b)
-    if(a==0 or b==0):
-        if(a!=0):
-            return (a-1)
-        else:
-            return (b-1)
-    else:
-        count=1
-        y=[1 if(s[i]==s[i+1]) else 0 for i in range(0,len(s)-1)]
-        return sum(y)
+    y=[1 if(s[i]==s[i+1]) else 0 for i in range(0,len(s)-1)]
+    return sum(y)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
