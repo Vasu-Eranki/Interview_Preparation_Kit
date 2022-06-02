e2b09c66a4fc510dcb1d07e470e3663bb3b92845
Vasu Eranki <vasueranki@gmail.com>
2022-06-01 19:04:30 -0700

    Update Two_Sum.py

--- LeetCode/Python/Two_Sum.py
+++ LeetCode/Python/Two_Sum.py
@@ -1,7 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        comb = []
-        for i in range(len(nums)):
-            for k in range(i+1,len(nums)):
-                if (nums[i] + nums[k]) == target:
-                    return [i,k]
+        for i in range(0,len(nums)):
+            target_number = target-nums[i]
+            if(target_number in nums):
+                for j in range(0,len(nums)):
+                    if(nums[j]==target_number) and (i!=j):
+                        return [i,j]
+            else:
+                continue
