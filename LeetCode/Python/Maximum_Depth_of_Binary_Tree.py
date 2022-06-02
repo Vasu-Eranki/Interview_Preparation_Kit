c21f66694a8d0b54a5e04eed6644c3336c88b996
Vasu Eranki <vasueranki@gmail.com>
2022-06-01 19:36:40 -0700

    Update Maximum_Depth_of_Binary_Tree.py

--- LeetCode/Python/Maximum_Depth_of_Binary_Tree.py
+++ LeetCode/Python/Maximum_Depth_of_Binary_Tree.py
@@ -6,20 +6,11 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        length = recursive_solution(root)
+        length = recursive_solution(root,0)
         return length
         
-def recursive_solution(root):
-    l_left = 0
-    l_right =0
+def recursive_solution(root,depth):
     if(not root):
-        return 0
-    elif(not root.left) and ( not root.right):
-        return 1
+        return depth
     else:
-        if(root.left):
-            l_left = 1+recursive_solution(root.left)
-        if(root.right):
-            l_right = 1+recursive_solution(root.right)
-    return max(l_left,l_right)
-    
+        return max(recursive_solution(root.left,depth+1),recursive_solution(root.right,depth+1))
