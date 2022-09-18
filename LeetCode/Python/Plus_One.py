1be84b8631c989cd25a9faa93aff81555f3fc630
Vasu Eranki <vasueranki@gmail.com>
2022-09-18 00:14:03 -0700

    Create Plus_One.py

---
+++ LeetCode/Python/Plus_One.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry =1
+        for i in range(len(digits)-1,-1,-1):
+            digits[i] = digits[i]+carry
+            carry = digits[i]//10
+            digits[i] = digits[i]%10
+        if(carry!=0):
+            digits.insert(0,carry)
+        return digits
