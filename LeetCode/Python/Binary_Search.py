7b004a9812370a882bb835bb57e46ffc4dc64b9e
Vasu Eranki <vasueranki@gmail.com>
2022-09-17 18:57:39 -0700

    Update Binary_Search.py

--- LeetCode/Python/Binary_Search.py
+++ LeetCode/Python/Binary_Search.py
@@ -1,8 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        index = -1
-        for i in range(0,len(nums)):
-            if(nums[i]==target):
-                index = i
-                break
+        index = binary_search(nums,target)
         return index
+        
+def binary_search(array,target):
+    index = 0
+    length = len(array)
+    if(length==1) and (array[0]!=target):
+        return -1
+    mid_length = length//2
+    actual_value = array[mid_length]
+    if(actual_value==target):
+        return mid_length
+    elif(actual_value>target):
+        index = binary_search(array[:mid_length],target)
+    else:
+        index =  binary_search(array[mid_length:],target)
+        if(index!=-1): index = index +mid_length
+    return index
